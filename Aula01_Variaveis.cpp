9b592eb320598b4322022caff666d90c8f96d2e8
amandafurlanetto01-lgtm <amandafurlanetto01@gmail.com>
2026-06-16 20:59:46 -0300

    Create Aula01_Variaveis.cpp
    Programa que receba a idade e altura do usuário.

---
+++ Aula01_Variaveis.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int idade;
+    double altura;
+
+    cout << "Digite sua idade: ";
+    cin >> idade;
+
+    cout << "Digite sua altura: ";
+    cin >> altura;
+
+    cout << "Idade: " << idade << " anos" << endl;
+    cout << "Altura: " << altura << " m" << endl;
+
+    return 0;
+}
