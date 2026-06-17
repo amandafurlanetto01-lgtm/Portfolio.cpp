23a78cb33e323e0ea5ed22255f598c446bd1b6ac
amandafurlanetto01-lgtm <amandafurlanetto01@gmail.com>
2026-06-17 12:47:09 -0300

    entradaesaida.cpp

---
+++ entradaesaida.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string nome;
+
+    cout << "Digite seu nome: ";
+    cin >> nome;
+
+    cout << "Ola, " << nome << "! Seja bem-vindo(a)." << endl;
+
+    return 0;
+}
