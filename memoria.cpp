24b3ddddfcc6a738a615f96feb6f91789e620464
amandafurlanetto01-lgtm <amandafurlanetto01@gmail.com>
2026-06-17 12:57:09 -0300

    Create memoria.cpp

---
+++ memoria.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int idade = 27;
+    double altura = 1.64;
+    char sexo = 'F';
+
+    cout << "Idade: " << idade << endl;
+    cout << "Altura: " << altura << endl;
+    cout << "Sexo: " << sexo << endl;
+
+    return 0;
+}
