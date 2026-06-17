6f91aa938945a0b8e96da8747bb71ea19102e345
amandafurlanetto01-lgtm <amandafurlanetto01@gmail.com>
2026-06-17 14:28:43 -0300

    Create tipagem.cpp

---
+++ tipagem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int numero;
+
+    cout << "Digite um numero: ";
+    cin >> numero;
+
+    cout << "Valor armazenado: " << numero << endl;
+
+    return 0;
+}
