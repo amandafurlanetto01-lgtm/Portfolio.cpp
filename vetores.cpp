26aad963f716dbce078bb9053c617407cab96096
amandafurlanetto01-lgtm <amandafurlanetto01@gmail.com>
2026-06-17 15:18:10 -0300

    Create vetores.cpp

---
+++ vetores.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numeros[5] = {10, 20, 30, 40, 50};
+
+    for(int i = 0; i < 5; i++) {
+        cout << numeros[i] << endl;
+    }
+
+    return 0;
+}
